e63d7ab6aa78d3b55e4929cba27040e6b2266bb4
R Karthik <r8491054@gmail.com>
2026-05-25 00:24:09 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,88 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+
+function addTransactionDOM(transaction){
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const item = document.createElement('li');
+
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+  item.innerHTML = `
+  ${transaction.text}
+  <span>${sign}₹${Math.abs(transaction.amount)}</span>
+  <button class="delete-btn" onclick="removeTransaction(${transactions.indexOf(transaction)})">x</button>
+`;
+
+  list.appendChild(item);
+}
+
+function updateValues(){
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => acc + item, 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => acc + item, 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts
+      .filter(item => item < 0)
+      .reduce((acc, item) => acc + item, 0) * -1
+  ).toFixed(2);
+
+  balance.innerText = `₹${total}`;
+  money_plus.innerText = `+₹${income}`;
+  money_minus.innerText = `-₹${expense}`;
+}
+
+function updateLocalStorage(){
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+function init(){
+  list.innerHTML = '';
+
+  transactions.forEach(addTransactionDOM);
+
+  updateValues();
+}
+
+function addTransaction(e){
+  e.preventDefault();
+
+  const transaction = {
+    text: text.value,
+    amount: +amount.value
+  };
+
+  transactions.push(transaction);
+
+  addTransactionDOM(transaction);
+
+  updateValues();
+
+  updateLocalStorage();
+
+  text.value = '';
+  amount.value = '';
+}
+function removeTransaction(index){
+  transactions.splice(index,1);
+
+  updateLocalStorage();
+  init();
+}
+
+form.addEventListener('submit', addTransaction);
+
+init();
